0ebf1103a9ce55bbd4d53bea3b606a2002ec624a
Karlosicolombia <47667519+Karlosicolombia@users.noreply.github.com>
2023-01-24 14:49:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
+"Authors for this project is Karl Rosengren and Theodor Ahrenius"
 
 import tensorflow as tf
+
 
 "Hello Theodor this is a test"
 a = "Tensorflow yes"
 print(a)
-
+print(tf.__version__)
 "Hello Hello"
