80c62849f6ab215e75be83c69cd79926d525163a
Karl Rosengren <47667519+Karlosicolombia@users.noreply.github.com>
2023-04-03 10:14:57 +0200

    Update yoloAnnotation.py

--- yoloAnnotation.py
+++ yoloAnnotation.py
@@ -1,10 +1,20 @@
 from DeepImageBlending import two_pass
-
+from DeepImageBlending import run
+import sys
 
 def annotateimage(image):
  
   id = 0  
-  xmin = 
-  ymin =
-  x =
-  y =
+  xmin = x_start
+  ymin = y_start
+  x = ss
+  y = ss
+  number = sourcefile[5]
+
+ file = open(str(number)'.txt', 'a')
+ sys.stdout = file
+
+ print(id xmin ymin x y) 
+
+
+ file.close()
